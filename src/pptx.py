3af8c13dc100fdd23cc9c87cb70655ea9826e14a
meenzoon <rlaalswns523@gmail.com>
2025-09-27 22:12:59 +0900

    file명 변경

--- src/pptx.py
+++ src/pptx.py
@@ -1,4 +1,4 @@
 # for local file
 from magic_doc.docconv import DocConverter, S3Config
 converter = DocConverter(s3_config=None)
-markdown_content, time_cost = converter.convert("../file/hadoop-설명자료.pptx", conv_timeout=300)
+markdown_content, time_cost = converter.convert("../file/test.pptx", conv_timeout=300)
