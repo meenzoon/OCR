9288433c27f168f98bb03df2dcb733a45df592f8
meenzoon <rlaalswns523@gmail.com>
2025-09-28 19:44:52 +0900

    os import

--- src/pptx.py
+++ src/pptx.py
@@ -1,4 +1,5 @@
 # for local file
+import os
 from magic_doc.docconv import DocConverter, S3Config
 converter = DocConverter(s3_config=None)
 markdown_content, time_cost = converter.convert("../file/test.pptx", conv_timeout=300)
